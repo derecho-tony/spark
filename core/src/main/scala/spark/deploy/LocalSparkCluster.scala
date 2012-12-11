51bfd52233a29266bf483a869e8ab92dd04f2609
Reynold Xin <rxin@cs.berkeley.edu>
2012-12-10 17:36:39 -0800

    1. Allow setting SPARK_TEST_LOCAL_HOSTS to provide local Spark cluster with a list of usable hostnames.
    2. Avoid sending block updates for shuffle blocks to the master when a
    slave's block manager comes back from hiatus.

--- core/src/main/scala/spark/deploy/LocalSparkCluster.scala
+++ core/src/main/scala/spark/deploy/LocalSparkCluster.scala
@@ -1,27 +1,28 @@
 package spark.deploy
 
+import scala.collection.mutable.ArrayBuffer
+
 import akka.actor.{ActorRef, Props, Actor, ActorSystem, Terminated}
 
+import spark.SparkException
 import spark.deploy.worker.Worker
 import spark.deploy.master.Master
 import spark.util.AkkaUtils
 import spark.{Logging, Utils}
 
-import scala.collection.mutable.ArrayBuffer
-
 private[spark]
 class LocalSparkCluster(numSlaves: Int, coresPerSlave: Int, memoryPerSlave: Int) extends Logging {
-  
+
   val localIpAddress = Utils.localIpAddress
-  
+
   var masterActor : ActorRef = _
   var masterActorSystem : ActorSystem = _
   var masterPort : Int = _
   var masterUrl : String = _
-  
+
   val slaveActorSystems = ArrayBuffer[ActorSystem]()
   val slaveActors = ArrayBuffer[ActorRef]()
-  
+
   def start() : String = {
     logInfo("Starting a local Spark cluster with " + numSlaves + " slaves.")
 
@@ -33,14 +34,33 @@ class LocalSparkCluster(numSlaves: Int, coresPerSlave: Int, memoryPerSlave: Int)
       Props(new Master(localIpAddress, masterPort, 0)), name = "Master")
     masterActor = actor
 
+    // By default, we assume 127.100.*.* are local hostnames that Spark can bind to. However,
+    // in some systems (e.g. some configuration of Mac OS X), 127.100.*.* is not usable names
+    // for localhost. In those cases, we allow the developer to specify a list of usable
+    // local hostnames in spark-env.sh using SPARK_TEST_LOCAL_HOSTS environmental variable.
+    // The list of hostnames should be separated by comma. Developers can edit /etc/hosts
+    // to generate a list (at least 2) of usable hostnames for Spark test suites. For example,
+    //
+    // export SPARK_TEST_LOCAL_HOSTS="localhost1, localhost2"
+    val localHosts: Array[String] =
+      if (System.getenv("SPARK_TEST_LOCAL_HOSTS") == null) {
+        // We can pretend to test distributed stuff by giving the slaves distinct hostnames.
+        // All of 127/8 should be a loopback, we use 127.100.*.* in hopes that it is
+        // sufficiently distinctive.
+        Array.tabulate[String](numSlaves) { slaveNum => "127.100.0." + (slaveNum + 1) }
+      } else {
+        val hosts = System.getenv("SPARK_TEST_LOCAL_HOSTS").split(".\\s+").take(numSlaves)
+        if (hosts.size < numSlaves) {
+          throw new SparkException(
+            "SPARK_TEST_LOCAL_HOSTS needs to specify at least " + numSlaves + " host names.")
+        }
+        hosts
+      }
+
     /* Start the Slaves */
-    for (slaveNum <- 1 to numSlaves) {
-      /* We can pretend to test distributed stuff by giving the slaves distinct hostnames.
-         All of 127/8 should be a loopback, we use 127.100.*.* in hopes that it is
-         sufficiently distinctive. */
-      val slaveIpAddress = "127.100.0." + (slaveNum % 256)
-      val (actorSystem, boundPort) = 
-        AkkaUtils.createActorSystem("sparkWorker" + slaveNum, slaveIpAddress, 0)
+    localHosts.zipWithIndex.foreach { case(slaveIpAddress, slaveNum) =>
+      val (actorSystem, boundPort) =
+        AkkaUtils.createActorSystem("sparkWorker" + (slaveNum + 1), slaveIpAddress, 0)
       slaveActorSystems += actorSystem
       val actor = actorSystem.actorOf(
         Props(new Worker(slaveIpAddress, boundPort, 0, coresPerSlave, memoryPerSlave, masterUrl)),
